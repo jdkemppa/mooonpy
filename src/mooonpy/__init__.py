d4ffb09c253933f8e04a9b3914e9c5d1d7c050fa
tkwavrun <106693788+tkwavrun@users.noreply.github.com>
2025-12-10 16:35:15 -0500

    uncommented __init__.py's

--- src/mooonpy/__init__.py
+++ src/mooonpy/__init__.py
@@ -1,34 +1,34 @@
-# # -*- coding: utf-8 -*-
-# import importlib
-# import os
-#
-# from .rcsetup import rcParams
-#
-# # Generate "aliased" imports
-# from .molspace.molspace import Molspace as Molspace
-# from .molspace import doc_examples as DocExamples
-#
-# from .thermospace.thermospace import Thermospace as Thermospace ## TDM
-# from .tools.file_utils import Path as Path ## TDM
-#
-#
-# from .rcsetup import rcParams
-#
-# __all__ = ['Molspace',
-#            'DocExamples',
-#            'Thermospace',
-#            'Path',
-# ]
-#
-#
-# # Get the current directory (the package root)
-# _package_dir = os.path.dirname(__file__)
-#
-#
-# # List of submodules/folders to import
-# _submodules = ['guis', 'molspace', 'programs', 'thermospace', 'tools', 'xrdspace','fitting']
-# for name in _submodules:
-#     if os.path.isdir(os.path.join(_package_dir, name)):
-#         module = importlib.import_module(f'.{name}', __package__)
-#         globals()[name] = module
-#         __all__.append(name)
+# -*- coding: utf-8 -*-
+import importlib
+import os
+
+from .rcsetup import rcParams
+
+# Generate "aliased" imports
+from .molspace.molspace import Molspace as Molspace
+from .molspace import doc_examples as DocExamples
+
+from .thermospace.thermospace import Thermospace as Thermospace ## TDM
+from .tools.file_utils import Path as Path ## TDM
+
+
+from .rcsetup import rcParams
+
+__all__ = ['Molspace',
+           'DocExamples',
+           'Thermospace',
+           'Path',
+]
+
+
+# Get the current directory (the package root)
+_package_dir = os.path.dirname(__file__)
+
+
+# List of submodules/folders to import
+_submodules = ['guis', 'molspace', 'programs', 'thermospace', 'tools', 'xrdspace','fitting']
+for name in _submodules:
+    if os.path.isdir(os.path.join(_package_dir, name)):
+        module = importlib.import_module(f'.{name}', __package__)
+        globals()[name] = module
+        __all__.append(name)
--- src/mooonpy/tools/__init__.py
+++ src/mooonpy/tools/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-# from .file_utils import *
-# from .tables import *
-# from .math_utils import *
-# from .string_utils import *
-# from .signals import *
-# from .loop_utils import *
-# from .misc_utils import *
+from .file_utils import *
+from .tables import *
+from .math_utils import *
+from .string_utils import *
+from .signals import *
+from .loop_utils import *
+from .misc_utils import *
